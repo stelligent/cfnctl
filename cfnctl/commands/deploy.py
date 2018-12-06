e2b4c2d1be3fd763e08559bc21623366e0edf73c
Michael Neil <mneil@mneilsworld.com>
2018-12-06 15:28:09 -0800

    Fix issue with describe stacks on stack that does not exist

--- cfnctl/commands/deploy.py
+++ cfnctl/commands/deploy.py
@@ -8,23 +8,27 @@ stack or update an existing stack
 import datetime
 import time
 import logging
-# import sys
 # import json
 import os
 import re
+import sys
 # import argparse
 import boto3
+import botocore.exceptions
 # from jinja2 import Environment, FileSystemLoader
 
-def _upload_template(simple_storage_service, bucket, template_name):
+def _upload_template(simple_storage_service, stack, bucket, template_name):
     '''Upload the cfn template
     to S3
     '''
     logging.info('Uploading template file')
     if _is_url(template_name):
-        return
+        return None
     file_path = os.path.abspath(template_name)
-    return simple_storage_service.upload_file(os.path.basename(template_name), bucket, file_path)
+    s3_path = '{}/{}'.format(
+        stack,
+        os.path.basename(template_name))
+    return simple_storage_service.upload_file(file_path, bucket, s3_path)
 
 
 def _bucket_exists(simple_storage_service, name):
@@ -51,20 +55,24 @@ def _maybe_make_bucket(simple_storage_service, region, account_id):
     return string - bucket name
     '''
     logging.info('Maybe make S3 bucket')
-    stack_bucket = 'landing-zone-template-bucket-{}-{}'.format(
+    stack_bucket = 'cfnctl-staging-bucket-{}-{}'.format(
         region, account_id)
     if _bucket_exists(simple_storage_service, stack_bucket):
         logging.info('Bucket exists')
         return stack_bucket
 
     logging.info('No S3 bucket found, creating %s', stack_bucket)
-    simple_storage_service.create_bucket(Bucket=stack_bucket)
+    simple_storage_service.create_bucket(
+        Bucket=stack_bucket,
+        CreateBucketConfiguration={
+            'LocationConstraint': region
+        })
     simple_storage_service.put_bucket_versioning(
         Bucket=stack_bucket,
         VersioningConfiguration={
             'MFADelete': 'Enabled',
             'Status': 'Enabled'
-        }
+        },
     )
     return stack_bucket
 
@@ -76,7 +84,17 @@ def _stack_exists(client, name):
     return bool
     '''
     logging.info('Verifying Stack exists')
-    stacks = client.describe_stacks(StackName=name)
+    stacks = None
+    try:
+        stacks = client.describe_stacks(StackName=name)
+    except botocore.exceptions.ClientError as error:
+        message = error.response.get('Error', {}).get('Message', 'Unknown')
+        if 'not exist' in message:
+            return False
+        print error
+    if not stacks:
+        sys.exit()
+
     exists = False
     for stack in stacks['Stacks']:
         if stack['StackName'] == name:
@@ -163,8 +181,8 @@ def _get_parameters():
     # )
     # return json.loads(rendered)
 
-def _is_url(str):
-    return re.match('https?://', str) is not None
+def _is_url(search):
+    return re.match('https?://', search) is not None
 
 def _get_template_url(bucket, template_name):
     '''Get the cfn template url
@@ -186,8 +204,8 @@ def deploy(args):
     simple_storage_service = boto3.client('s3')
     account_id = boto3.client('sts').get_caller_identity().get('Account')
     region = args.region or boto3.session.Session().region_name
-    bucket = _maybe_make_bucket(simple_storage_service, region, account_id)
-    _upload_template(simple_storage_service, bucket, args.template)
+    bucket = args.bucket or _maybe_make_bucket(simple_storage_service, region, account_id)
+    _upload_template(simple_storage_service, stack, bucket, args.template)
     changeset = _make_change_set(client, stack,
                                  _get_template_url(bucket, args.template), _get_parameters())
     ready = _wait_for_changeset(client, changeset, stack)
